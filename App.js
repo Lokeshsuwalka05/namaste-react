e4d5efeca3d9dec46bf34f7da46a75d8260ecd93
unknown <lokeshsuwalka2002@gmail.com>
2025-07-24 01:25:35 +0530

    Dynamic Layout Done

--- App.js
+++ App.js
@@ -32,43 +32,665 @@ const Header = () => {
   );
 };
 
-const ResCard = () => {
+const ResCard = (props) => {
+  console.log(props);
   return (
     <div className="card">
       
         <div className="food-image">
-          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/1/9/f2ac1fad-bff8-4a78-bc9b-f65567f93906_249749.JPG"></img>
+          <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+props.resObj.info.cloudinaryImageId}></img>
         </div>
+        <div className="res-name">{props.resObj.info.name}</div>
         <div className="star-rating-time">
           <div className="star">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc3yCfMItzK5tavoiWhSwT-EBBIXW8GTwd7A&s"></img>
           </div>
           <div className="rating-time">
-            <span>4.3 </span>
-            40-45 mins
+            <span>{props.resObj.info.avgRating} </span>
+            {props.resObj.info.sla.slaString}
           </div>
         </div>
-        <div className="cuisine">American,Chinese</div>
-        <div className="address">Vivek Vihar</div>
+        <div className="cuisine">{props.resObj.info.cuisines.join(",")}</div>
+        <div className="address">{props.resObj.info.areaName}</div>
     </div>
   );
 };
+const restaurants= [
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "898253",
+"name": "KFC",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/4ea73ddf-2cf4-4a69-a9d9-89db9f6c9ae1_898253.JPG",
+"locality": "Sunshine Sheodan Heights",
+"areaName": "Sodala",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Burgers",
+"Biryani",
+"American",
+"Snacks",
+"Fast Food"
+],
+"avgRating": 4.1,
+"parentId": "547",
+"avgRatingString": "4.1",
+"totalRatingsString": "828",
+"sla": {
+"deliveryTime": 21,
+"lastMileTravel": 3.8,
+"serviceability": "SERVICEABLE",
+"slaString": "20-25 mins",
+"lastMileTravelString": "3.8 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-24 01:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹59"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2f995b6e-5607-4948-b072-fe4e8c8e748e"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/jaipur/kfc-sunshine-sheodan-heights-sodala-rest898253",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "764055",
+"name": "Theobroma",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/23/709f8254-7082-4d8c-a6b3-6f080a6602f9_764055.JPG",
+"locality": "Vaishali Nagar",
+"areaName": "C Scheme",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Bakery",
+"Desserts"
+],
+"avgRating": 4.6,
+"parentId": "1040",
+"avgRatingString": "4.6",
+"totalRatingsString": "2.0K+",
+"sla": {
+"deliveryTime": 15,
+"lastMileTravel": 2.4,
+"serviceability": "SERVICEABLE",
+"slaString": "10-15 mins",
+"lastMileTravelString": "2.4 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-24 02:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png",
+"description": "bolt!"
+},
+{
+"imageId": "newg.png",
+"description": "Gourmet"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png"
+}
+},
+{
+"attributes": {
+"description": "Gourmet",
+"imageId": "newg.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹99"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "3.1",
+"ratingCount": "60"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2f995b6e-5607-4948-b072-fe4e8c8e748e"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/jaipur/theobroma-vaishali-nagar-c-scheme-rest764055",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "53744",
+"name": "Pizza Hut",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/4f2f9d35-895a-4f66-b015-d38bf551cac4_53744.JPG",
+"locality": "Bais Godam",
+"areaName": "Bais Godam",
+"costForTwo": "₹350 for two",
+"cuisines": [
+"Pizzas"
+],
+"avgRating": 4.1,
+"parentId": "721",
+"avgRatingString": "4.1",
+"totalRatingsString": "6.5K+",
+"sla": {
+"deliveryTime": 22,
+"lastMileTravel": 2.5,
+"serviceability": "SERVICEABLE",
+"slaString": "20-25 mins",
+"lastMileTravelString": "2.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-24 04:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"aggregatedDiscountInfoV2": {},
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "3.8",
+"ratingCount": "391"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2f995b6e-5607-4948-b072-fe4e8c8e748e"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/jaipur/pizza-hut-bais-godam-rest53744",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "254590",
+"name": "NIC Ice Creams",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/7/8/1b6dc5f2-db4c-47d2-be3e-3f4da19326f2_254590.JPG",
+"locality": "Shri Ram Colony",
+"areaName": "Sindhi Camp",
+"costForTwo": "₹120 for two",
+"cuisines": [
+"Ice Cream",
+"Desserts"
+],
+"avgRating": 4.6,
+"veg": true,
+"parentId": "6249",
+"avgRatingString": "4.6",
+"totalRatingsString": "5.5K+",
+"sla": {
+"deliveryTime": 18,
+"lastMileTravel": 2.3,
+"serviceability": "SERVICEABLE",
+"slaString": "15-20 mins",
+"lastMileTravelString": "2.3 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-24 00:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png",
+"description": "bolt!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹124"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2f995b6e-5607-4948-b072-fe4e8c8e748e"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/jaipur/nic-ice-creams-shri-ram-colony-sindhi-camp-rest254590",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "249749",
+"name": "McDonald's",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/9/f2ac1fad-bff8-4a78-bc9b-f65567f93906_249749.JPG",
+"locality": "Civil Lines",
+"areaName": "Civil Lines",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"American"
+],
+"avgRating": 4.3,
+"parentId": "630",
+"avgRatingString": "4.3",
+"totalRatingsString": "23K+",
+"sla": {
+"deliveryTime": 36,
+"lastMileTravel": 2.5,
+"serviceability": "SERVICEABLE",
+"slaString": "35-40 mins",
+"lastMileTravelString": "2.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-24 01:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Ratnesh_Badges/Rx_Awards_2025/Bolt.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Ratnesh_Badges/Rx_Awards_2025/Bolt.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹39"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "3.9",
+"ratingCount": "1.4K+"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2f995b6e-5607-4948-b072-fe4e8c8e748e"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/jaipur/mcdonalds-civil-lines-rest249749",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "90186",
+"name": "Burger King",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/9c75fd37-f428-4a2f-9beb-ba1f2e2cf391_90186.jpg",
+"locality": "Amrapali Marg",
+"areaName": "Vaishali Nagar",
+"costForTwo": "₹350 for two",
+"cuisines": [
+"Burgers",
+"American"
+],
+"avgRating": 4.2,
+"parentId": "166",
+"avgRatingString": "4.2",
+"totalRatingsString": "44K+",
+"sla": {
+"deliveryTime": 36,
+"lastMileTravel": 6.1,
+"serviceability": "SERVICEABLE",
+"slaString": "35-40 mins",
+"lastMileTravelString": "6.1 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-24 04:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Rxawards/_CATEGORY-Burger.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-Burger.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹59"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2f995b6e-5607-4948-b072-fe4e8c8e748e"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/jaipur/burger-king-amrapali-marg-vaishali-nagar-rest90186",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "735128",
+"name": "Big Bowl",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/22/30548ac4-2e57-4c1d-ba73-8372f74a1ad2_735128.JPG",
+"locality": "Siddhi Vinayak Complex",
+"areaName": "Vivek Vihar",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Chinese",
+"Tibetan",
+"Desserts"
+],
+"avgRating": 4.3,
+"parentId": "434792",
+"avgRatingString": "4.3",
+"totalRatingsString": "1.9K+",
+"sla": {
+"deliveryTime": 29,
+"lastMileTravel": 3.6,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "3.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-24 01:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹129"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2f995b6e-5607-4948-b072-fe4e8c8e748e"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/jaipur/big-bowl-siddhi-vinayak-complex-vivek-vihar-rest735128",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+}
+]
 
 const Body = () => {
   return (
     <div className="body">
       <div className="Search">Search</div>
       <div className="card-container">
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
+       {
+        restaurants.map((resCard,index)=>{
+          return <ResCard key={index} resObj={resCard}></ResCard>
+        })
+       }
+        
       </div>
     </div>
   );
