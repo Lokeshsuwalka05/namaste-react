492a7094aab48730315fcba56a8c9861a0cd89fd
unknown <lokeshsuwalka2002@gmail.com>
2025-08-02 20:25:15 +0530

    Updated Routes

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
+import { createBrowserRouter,RouterProvider } from "react-router";
+import Contact from "./src/components/Contact";
+import About from "./src/components/About";
+import Error from "./src/components/Error";
 
 const Applayout = () => {
   return (
@@ -11,7 +15,24 @@ const Applayout = () => {
     </div>
   );
 };
+const appRouter=createBrowserRouter([
+  { 
+    path:"/",
+    element:<Applayout/>,
+    errorElement:<Error/>
+  },
+  {
+    path:"/About",
+    element:<About/>
+
+  },
+  {
+    path:"/Contact",
+    element:<Contact/>
+
+  }
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading1);
-root.render(<Applayout />);
+root.render(<RouterProvider router={appRouter}/>);
