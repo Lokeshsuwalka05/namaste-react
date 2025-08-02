1298f55af9cf55365beb01e6673f83d15dc228ca
unknown <lokeshsuwalka2002@gmail.com>
2025-08-02 16:25:09 +0530

    Added Swiggy API

--- src/utills/constants.js
+++ src/utills/constants.js
@@ -1,3 +1,4 @@
 export const LOGO_URL="https://i.pinimg.com/736x/83/c6/a9/83c6a9427e6c36857ef6cbc10c9e9565.jpg";
 export const CLOUDINARY_IMG_CDN="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const star_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc3yCfMItzK5tavoiWhSwT-EBBIXW8GTwd7A&s"
+export const Swiggy_API="https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.91360&lng=75.78580&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
