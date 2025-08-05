75fd0dba53e2e68951aa4d3b077aab50285b8ae7
unknown <lokeshsuwalka2002@gmail.com>
2025-08-05 16:09:09 +0530

    Added Outlet and importing RestMenu

--- App.js
+++ App.js
@@ -2,37 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
-import { createBrowserRouter,RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import Contact from "./src/components/Contact";
 import About from "./src/components/About";
 import Error from "./src/components/Error";
+import { Outlet } from "react-router";
+import RestaurantMenu from "./src/components/RestaurantMenu";
 
 const Applayout = () => {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <Outlet/>
     </div>
   );
 };
-const appRouter=createBrowserRouter([
-  { 
-    path:"/",
-    element:<Applayout/>,
-    errorElement:<Error/>
-  },
+const appRouter = createBrowserRouter([
   {
-    path:"/About",
-    element:<About/>
-
+    path: "/",
+    element: <Applayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body/>,
+      },
+      {
+        path: "/About",
+        element: <About />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+      {
+        path:"/Restaurants/:resid",
+        element:<RestaurantMenu/>
+      }
+    ],
+    errorElement: <Error />,
   },
-  {
-    path:"/Contact",
-    element:<Contact/>
-
-  }
-
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter}/>);
+root.render(<RouterProvider router={appRouter} />);
