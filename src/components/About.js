70fb4adf8a68357c4241fda4502dad566fd4a88e
unknown <lokeshsuwalka2002@gmail.com>
2025-08-06 20:46:10 +0530

    Saw the React Life Cycle

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,26 @@
-const About=()=>{
-  return(
-    <>
-    <h1>Hello Guys</h1>
-    <p>This is the Determination Of Lokesh</p>
-    </>
-  )
+import User from "./User";
+import React from "react";
+import UserClass from "./UserClass";
+class About extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("parent constructor");
+
+  }
+  componentDidMount(){
+    console.log("parent componentDidMount ");
+  }
+  render(){
+    console.log("parent rendered");
+    return(
+      <>
+        <h1>Hello Guys</h1>
+        <p>This is the Determination Of Lokesh</p>
+        {/* <User name={"lokesh suwalka"} location={"Jaipur"} contact={"@LokeshSuwalka05"}/> */}
+        <UserClass name={"lokesh suwalka"} location={"Jaipur"} contact={"@LokeshSuwalka05"}/>
+      </>
+    )
+  }
 }
+
 export default About;
