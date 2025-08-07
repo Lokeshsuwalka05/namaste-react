419da67afefe40a12ed18cd3fbf995f1c3c80ffa
unknown <lokeshsuwalka2002@gmail.com>
2025-08-07 20:15:52 +0530

    Saw ClassBased Components and RL

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,32 +1,42 @@
 import React from "react";
+import Shimmer from "./Shimmer";
 
 class UserClass extends React.Component {
   constructor(props){
     super(props);
     this.state={
-      count:0,
-      count1:1
+      userInfo:null
     }
-     console.log("child constructor");
+     console.log(this.props.name+"child constructor called");
+    
   }
-  componentDidMount(){
-    console.log("child componentDidMount ");
+  async componentDidMount(){
+    console.log(this.props.name+"child componentDidMount ");
+    const data=await fetch("https://api.github.com/users/Lokeshsuwalka05");
+    const json=await data.json();
+    this.setState({
+         userInfo:json
+    })
+  }
+  componentDidUpdate(){
+    console.log(this.props.name+"componentDidUpdate called");
+  }
+  componentWillUnmount(){
+    console.log(this.props.name+"componentWillUnmount called");
   }
   render() {
-     console.log("child rendered");
-    const {count,count1}=this.state
+    console.log(this.props.name+"child rendered");
+    if(this.state.userInfo==null){
+      return <Shimmer/>
+    }
+     console.log(this.state.userInfo);
+    const {name,followers,following,avatar_url}=this.state.userInfo;
     return (
       <div className="user-class-card" style={{border:"2px solid black"}}>
-        <p>Name:{this.props.name}</p>
-        <p>Location:{this.props.location}</p>
-        <p>Contact:{this.props.contact}</p>
-        <p>Count:{count}</p>
-         <button onClick={()=>{
-          //never update state variables directly
-           this.setState({
-            count:this.state.count+1
-           })
-         }}>Increase count</button>
+        <p>Name:{name}</p>
+        <img src={avatar_url}></img>
+        <p>Followers:{followers}</p>
+        <p>Following:{following}</p>
         <p></p>
       </div>
     );
