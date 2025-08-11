efdfd3bceaa894b4435ce0d3b897f6e6552791db
unknown <lokeshsuwalka2002@gmail.com>
2025-08-11 18:26:41 +0530

    Implemented Lazy Loading for Grocery

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
+import { lazy,Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Contact from "./src/components/Contact";
 import About from "./src/components/About";
 import Error from "./src/components/Error";
 import { Outlet } from "react-router";
 import RestaurantMenu from "./src/components/RestaurantMenu";
+
+const Grocery=lazy(()=>{
+  return import("./src/components/Grocery");
+})
 
 const Applayout = () => {
   return (
@@ -37,6 +42,10 @@ const appRouter = createBrowserRouter([
       {
         path:"/Restaurants/:resid",
         element:<RestaurantMenu/>
+      },
+      {
+        path:"/Grocery",
+        element:<Suspense fallback={<h1>Loading....</h1>}><Grocery/></Suspense>
       }
     ],
     errorElement: <Error />,
