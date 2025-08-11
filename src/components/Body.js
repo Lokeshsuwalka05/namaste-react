5f86a716ab09091b764a0420eb27d1e7cfb4ff07
unknown <lokeshsuwalka2002@gmail.com>
2025-08-11 18:33:51 +0530

    Fetching the data from useBody() hook and checking Online status by using useOnlineStatus Hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,41 +1,23 @@
 import ResCard from "./ResCard";
 // import resList from "../utills/mockdata";
-import { useState, useEffect } from "react";
+import { use, useState } from "react";
 import Shimmer from "./Shimmer";
-import { ResCard_API } from "../utills/constants";
-import { Link, useParams } from "react-router";
-import { useParams } from "react-router";
-
-
+import { Link } from "react-router";
+import useBody from "../utills/useBody";
+import useOnlineStatus from "../utills/useOnlineStatus";
 
 const Body = () => {
   // local state variable->super powerful variable
-  const [ListOfRestaurants, setListOfRestaurants] = useState([]);
-  const [filteredRestaurants, setfilteredRestaurants] = useState([]);
   const [searchText, setsearchText] = useState("");
-  const params=useParams();
-  console.log(params);
- 
+  const { ListOfRestaurants, filteredRestaurants,setfilteredRestaurants } = useBody();
+  const onlineStatus = useOnlineStatus();
+  if (onlineStatus === false) {
+    return <h1>You Are Offline,Please Connect to the Network</h1>;
+  }
   // const arr=useState(resList);
   // const Restaurants=arr[0];
   // const setRestaurants=arr[1];
   // - callback function of useEffect will be called after the componet has been rendered.
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const fetchData = async () => {
-    const data = await fetch(ResCard_API);
-    const json = await data.json();
-    // use optional chaining here
-    setListOfRestaurants(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-    setfilteredRestaurants(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-  };
-
   //conditional rendering:-A rendering based on a condition.
   // if(Restaurants.length===0){
   //   return <Shimmer/>
@@ -46,21 +28,20 @@ const Body = () => {
     <Shimmer />
   ) : (
     <div className="body">
-      
-
       <div className="Search-Functionality">
         <button
-        className="filter-btn"
-        onClick={() => {
-          const filterList = ListOfRestaurants.filter(
-            (res) => res.info.avgRating > 4.2
-          );
-          setfilteredRestaurants(filterList);
-          console.log("clicked");
-        }}
-      >
-        Top Rated Restaurants
-      </button>
+          className="filter-btn"
+          onClick={() => {
+            const filterList = ListOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.2
+            );
+            setfilteredRestaurants(filterList);
+            console.log("clicked");
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+
         <input
           className="search-field"
           value={searchText}
@@ -87,7 +68,12 @@ const Body = () => {
       <div className="card-container">
         {filteredRestaurants.map((restaurant) => {
           return (
-            <Link key={restaurant.info.id} to={"/Restaurants/"+restaurant.info.id}><ResCard resObj={restaurant} ></ResCard></Link>
+            <Link
+              key={restaurant.info.id}
+              to={"/Restaurants/" + restaurant.info.id}
+            >
+              <ResCard resObj={restaurant}></ResCard>
+            </Link>
           );
         })}
       </div>
