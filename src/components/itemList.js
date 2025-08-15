dc8abc27e47335fe5d7167931f60d23a753811d8
unknown <lokeshsuwalka2002@gmail.com>
2025-08-15 21:05:43 +0530

    .

--- src/components/itemList.js
+++ src/components/itemList.js
@@ -1,6 +1,30 @@
-const itemList=()=>{
+const ItemList = (item) => {
+  const { title } = item.item;
+  const { itemCards } = item.item;
+  console.log(item);
   return (
-     <h1>items</h1>
-  )
-}
-export default itemList;
+    <>
+      <div className="border border-gray-600 m-3 p-2">
+        <div className="flex justify-between m-2 p-2 border">
+          <span>
+            <h1>{title + "(" + itemCards.length + ")"}</h1>
+          </span>
+          <span>⬇️</span>
+        </div>
+        <div>
+          {itemCards.map((item) => {
+            const { name, description } = item.card.info;
+            return (
+              <>
+                <div className="">
+                   
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+export default ItemList;
