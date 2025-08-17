7dc939e2d7cff7ca68358f468dcf10c1fd0bb81e
unknown <lokeshsuwalka2002@gmail.com>
2025-08-17 15:58:13 +0530

    Made a Accordian

--- src/components/MenuCategory.js
+++ src/components/MenuCategory.js
@@ -1,21 +1,22 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 const MenuCategory = (category) => {
-  console.log(category);
   const { title, itemCards } = category.category;
+  const [showItems,setShowItems]=useState(false);
   const handleClick=()=>{
-    return console.log("clicked");
+    setShowItems(!showItems);
   }
   return (
-    <div className="" onClick={handleClick}>
-      <div className="flex justify-between">
+    <div className="border-b-8  border-gray-300 mb-5">
+      <div className="flex justify-between mb-5 cursor-pointer" onClick={handleClick}>
         <span>
-          <h1>{title + "(" + itemCards.length + ")"}</h1>
+          <h1 className="font-bold text-black">{title + "(" + itemCards.length + ")"}</h1>
         </span>
         <span>⬇️</span>
       </div>
       <div>
         {category.category.itemCards.map((item,index) => {
-          return <ItemList key={index} item={item}></ItemList>;
+          return showItems&&<ItemList key={index} item={item}></ItemList>;
         })}
       </div>
     </div>
