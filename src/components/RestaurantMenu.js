356cea759a578e2c209f185cd460e82ad06a71a7
unknown <lokeshsuwalka2002@gmail.com>
2025-08-17 15:59:58 +0530

    Added A startRating Icon

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,5 +1,5 @@
 import Shimmer from "./Shimmer";
-import ItemCards from "./ItemCards";
+import MenuCategory from "./MenuCategory";
 import { useParams } from "react-router";
 import useRestaurantMenu from "../utills/useRestaurantMenu";
 
@@ -22,35 +22,38 @@ const RestaurantMenu = () => {
     areaName,
     sla,
   } = resInfo?.cards[2]?.card?.card?.info;
-  // const { itemCards } =
-  //   resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[3].card.card;
-  // console.log(itemCards);
+  console.log(resInfo?.cards[2]?.card?.card?.info);
+  // resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[3].card.card;
+  // console.log(resInfo);
+  // console.log(resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR);
+  const { cards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR;
+  //4 or 5
+  // console.log(cards);
   return (
     <div className="w-6/12 m-auto">
       <h1 className="font-bold text-3xl mb-9">{name}</h1>
       <div className="border border-gray-200 pl-4 pr-4 pb-4 rounded-4xl bg-gray-100 ">
         <div className="border border-gray-200 rounded-2xl p-4 shadow-lg  bg-white">
           <h3>
+            ⭐
             {avgRating + "(" + totalRatings + " ratings" + ")"}-
             {costForTwoMessage}
           </h3>
           <h3>{cuisines.join(",")}</h3>
-          <span style={{ fontSize: "25px" }}>
-            <strong>Outlet</strong> {areaName}
+          <span className="font-bold">
+            Outlet <span className="font-normal">{areaName}</span>
           </span>
           <h3>{sla.slaString}</h3>
         </div>
       </div>
       <div className="ResMenu">
-        <h2>Menu</h2>
-        <ItemCards
-          itemCard={
-            resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]
-              ?.card?.card.itemCards ||
-            resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]
-              ?.card?.card.itemCards
-          }
-        ></ItemCards>
+        <h2 className="flex justify-center font-serif text-gray-500 m-3 p-3 tracking-[0.3em]">MENU</h2>
+
+        {cards.filter(
+          (c) =>
+            c?.card?.card["@type"] ===
+            "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+        ).map((c,index)=><MenuCategory key={index} category={c.card.card}></MenuCategory>)}
       </div>
     </div>
   );
