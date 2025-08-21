89b76909f91d5ced3ac660665dcc00d237ea74ea
unknown <lokeshsuwalka2002@gmail.com>
2025-08-21 14:53:50 +0530

    Made changes in Accordian

--- src/components/MenuCategory.js
+++ src/components/MenuCategory.js
@@ -1,21 +1,26 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
-const MenuCategory = (category) => {
-  const { title, itemCards } = category.category;
-  const [showItems,setShowItems]=useState(false);
+const MenuCategory = ({category,showItems,setShowIndex,index}) => {
+  const { title, itemCards } = category;
+  // const [showItems,setShowItems]=useState(false);
   const handleClick=()=>{
-    setShowItems(!showItems);
+    if(showItems==true){
+       setShowIndex(null);
+    }
+    else{
+       setShowIndex(index);
+    }
   }
   return (
-    <div className="border-b-8  border-gray-300 mb-5">
-      <div className="flex justify-between mb-5 cursor-pointer" onClick={handleClick}>
+    <div className="border-b-8  border-gray-300 mb-5" onClick={handleClick}>
+      <div className="flex justify-between mb-5 cursor-pointer">
         <span>
           <h1 className="font-bold text-black">{title + "(" + itemCards.length + ")"}</h1>
         </span>
         <span>⬇️</span>
       </div>
       <div>
-        {category.category.itemCards.map((item,index) => {
+        {itemCards.map((item,index) => {
           return showItems&&<ItemList key={index} item={item}></ItemList>;
         })}
       </div>
