005d511daf8b81b8b6f1c95200b0d0927857d0c2
unknown <lokeshsuwalka2002@gmail.com>
2025-08-29 19:16:37 +0530

    added mock for resCard to testing

---
+++ src/mocks/mockData.js
@@ -0,0 +1,70 @@
+const mockData={
+   
+    "info": {
+        "id": "47595",
+        "name": "Pizza Hut",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/fea8a61d-8884-4039-b237-857292184839_47595.JPG",
+        "locality": "C Scheme",
+        "areaName": "MI Road",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+            "Pizzas"
+        ],
+        "avgRating": 4.1,
+        "parentId": "721",
+        "avgRatingString": "4.1",
+        "totalRatingsString": "8.7K+",
+        "sla": {
+            "deliveryTime": 37,
+            "lastMileTravel": 2,
+            "serviceability": "SERVICEABLE",
+            "slaString": "35-40 mins",
+            "lastMileTravelString": "2.0 km",
+            "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+            "nextCloseTime": "2025-08-29 23:00:00",
+            "opened": true
+        },
+        "badges": {},
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+            "entityBadges": {
+                "imageBased": {},
+                "textBased": {},
+                "textExtendedBadges": {}
+            }
+        },
+        "aggregatedDiscountInfoV3": {
+            "header": "ITEMS",
+            "subHeader": "AT ₹99"
+        },
+        "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+                "lottie": {},
+                "video": {}
+            }
+        },
+        "reviewsSummary": {},
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {},
+        "externalRatings": {
+            "aggregatedRating": {
+                "rating": "--"
+            }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+        "context": "seo-data-a5145e97-2311-464b-adcb-6a05f5862fae"
+    },
+    "cta": {
+        "link": "https://www.swiggy.com/city/jaipur/pizza-hut-c-scheme-mi-road-rest47595",
+        "type": "WEBLINK"
+    }
+
+}
+
+export default mockData;
