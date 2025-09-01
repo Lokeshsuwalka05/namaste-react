53f021b1a4ac2421f0eaf070193ee5a3a139cebe
unknown <lokeshsuwalka2002@gmail.com>
2025-09-01 20:34:51 +0530

    .

--- src/components/ResCard.js
+++ src/components/ResCard.js
@@ -1,19 +1,23 @@
 import { CLOUDINARY_IMG_CDN } from "../utills/constants";
 import { star_icon } from "../utills/constants";
 
-const ResCard = (props) => {
+const ResCard = ({ resObj }) => {
   // const {info}=props.resObj;
   // const obj=info;
   // const obj=props.resObj.info;
+  // console.log(props);
   const { cloudinaryImageId, name, avgRating, cuisines, areaName, sla } =
-    props.resObj.info;
+    resObj.info;
   const cuisines_text = cuisines.join(",");
   const display_text =
     cuisines_text.length > 30
       ? cuisines_text.slice(0, 30) + "..."
       : cuisines_text;
   return (
-    <div className="max-w-fit h-[330px] bg-white  border border-gray-200 rounded-lg shadow-sm p-3 hover:bg-gray-100 font-light hover:border-2">
+    <div
+      className="max-w-fit h-[330px] bg-white  border border-gray-200 rounded-lg shadow-sm p-3 hover:bg-gray-100 font-light hover:border-2"
+      data-testid="ResCard"
+    >
       <div className="food-image ">
         <img
           src={CLOUDINARY_IMG_CDN + cloudinaryImageId}
@@ -38,13 +42,15 @@ const ResCard = (props) => {
 };
 
 export const withDiscountLabel = (Resta) => {
-  return (props) => {
-    console.log(props);
-    const {header,subHeader}=props?.resObj?.info.aggregatedDiscountInfoV3;
+  return ({ resObj }) => {
+    // console.log(props);
+    const { header, subHeader } = resObj?.info.aggregatedDiscountInfoV3;
     return (
       <div className="relative">
-        <label className="absolute font-extrabold text-white top-44 left-8">{header+" "+subHeader}</label>
-        <ResCard {...props} />
+        <label className="absolute font-extrabold text-white top-44 left-8">
+          {header + " " + subHeader}
+        </label>
+        <ResCard {...{ resObj }} />
       </div>
     );
   };
