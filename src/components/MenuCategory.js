7e2705e0d048169da6dc042555e610ee4eef6eb1
unknown <lokeshsuwalka2002@gmail.com>
2025-09-01 20:34:19 +0530

    .

--- src/components/MenuCategory.js
+++ src/components/MenuCategory.js
@@ -1,28 +1,31 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
-const MenuCategory = ({category,showItems,setShowIndex,index}) => {
+import ItemCards from "./ItemCards";
+const MenuCategory = ({ category, showItems, setShowIndex, index }) => {
   const { title, itemCards } = category;
   // const [showItems,setShowItems]=useState(false);
-  const handleClick=()=>{
-    if(showItems==true){
-       setShowIndex(null);
+  const handleClick = () => {
+    if (showItems == true) {
+      setShowIndex(null);
+    } else {
+      setShowIndex(index);
     }
-    else{
-       setShowIndex(index);
-    }
-  }
+  };
   return (
-    <div className="border-b-8  border-gray-300 mb-5" onClick={handleClick}>
-      <div className="flex justify-between mb-5 cursor-pointer">
+    <div className="border-b-8  border-gray-300 mb-5">
+      <div
+        className="flex justify-between mb-5 cursor-pointer"
+        onClick={handleClick}
+      >
         <span>
-          <h1 className="font-bold text-black">{title + "(" + itemCards.length + ")"}</h1>
+          <h1 className="font-bold text-black">
+            {title + "(" + itemCards.length + ")"}
+          </h1>
         </span>
         <span>⬇️</span>
       </div>
       <div>
-        {itemCards.map((item,index) => {
-          return showItems&&<ItemList key={index} item={item}></ItemList>;
-        })}
+        {<ItemCards itemCards={itemCards} showItems={showItems}></ItemCards>}
       </div>
     </div>
   );
