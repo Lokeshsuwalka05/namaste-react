3be3c2cf62f8401c3efbd3d1d29e8b67e7d16692
unknown <lokeshsuwalka2002@gmail.com>
2025-09-02 13:25:26 +0530

    .

--- src/components/itemList.js
+++ src/components/itemList.js
@@ -1,16 +1,27 @@
 import { useState } from "react";
 import { CLOUDINARY_IMG_CDN } from "../utills/constants";
-const ItemList = ({item}) => {
+import { useDispatch } from "react-redux";
+import { addItem } from "../utills/cartSlice";
+import Shimmer from "./Shimmer";
+const Item = ({ item }) => {
   const [expanded, setexpanded] = useState(false);
   const [more, setmore] = useState("more");
   const { name, description, price, imageId, defaultPrice } = item.card.info;
   const shortText = description.slice(0, 150);
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    //dispatch an action
+    dispatch(addItem(item));
+  };
   return (
     <>
-      <div className=" flex border-b-2 border-gray-300 mt-6 pb-4">
+      <div
+        className=" flex border-b-2 border-gray-300 mt-6 pb-4"
+        data-testid="item"
+      >
         <div className="w-8/12">
           <h1 className="font-bold">{name}</h1>
-          <p className="font-semibold">{price / 100||defaultPrice/100}</p>
+          <p className="font-semibold">{price / 100 || defaultPrice / 100}</p>
           {expanded ? description : shortText}
           {description.length > 150 && (
             <span
@@ -21,7 +32,12 @@ const ItemList = ({item}) => {
             </span>
           )}
         </div>
-        <div className="w-4/12">
+        <div className="relative w-4/12">
+          <div className="border border-black w-28 absolute -bottom-2 font-bold text-green-400 bg-white rounded-2xl left-7 flex justify-center">
+            <button className="cursor-pointer" onClick={handleClick}>
+              +ADD
+            </button>
+          </div>
           <img
             src={CLOUDINARY_IMG_CDN + imageId}
             className="rounded-2xl size-40"
@@ -31,4 +47,4 @@ const ItemList = ({item}) => {
     </>
   );
 };
-export default ItemList;
+export default Item;
